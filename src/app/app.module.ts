7e4a856b0dbe089fa2ce6475d61029c6fe38cc4a
Jørgen <jorghus@gmail.com>
2022-12-26 14:12:39 +0100

    created spa component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpaComponent } from './SPA/spa/spa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpaComponent
   ],
   imports: [
     BrowserModule,
