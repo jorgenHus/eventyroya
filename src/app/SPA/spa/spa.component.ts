e3bab405a75ddc0f996da1e6a5eaed7dc1c73849
Jørgen <jorghus@gmail.com>
2022-12-26 17:26:21 +0100

    story 1 up and running

--- src/app/SPA/spa/spa.component.ts
+++ src/app/SPA/spa/spa.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-spa',
   templateUrl: './spa.component.html',
   styleUrls: ['./spa.component.scss']
 })
-export class SpaComponent {
+export class SpaComponent implements OnInit{
+  bgColor = 'black';
+  output = 'Click on an image to change the output';
 
+  output$ = new BehaviorSubject<string>('menu');
+
+
+  ngOnInit(): void {
+    this.output$.next('menu');
+  }
+  changeOutput(clicked: string) {
+    this.output$.next(clicked);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpaComponent } from './SPA/spa/spa.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoriesComponent } from './SPA/stories/stories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpaComponent
+    SpaComponent,
+    StoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
